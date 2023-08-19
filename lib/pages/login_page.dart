25fbcc8b4aea19132e7d9baad14c7a1f21df06e7
Yojith Kaustabh <yojith212@gmail.com>
2023-08-19 18:26:03 +0530

    day 4 completed, in two days

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,17 +4,54 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontFamily: "Ariel",
-              fontSize: 50,
-              color: Colors.amber,
-              fontWeight: FontWeight.bold),
-          textScaleFactor: 0.98,
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset("assets/images/Login_Page.jpeg", fit: BoxFit.cover),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome To Rana, An Ayush App",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Text("Time to buy your own Rana");
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom())
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
